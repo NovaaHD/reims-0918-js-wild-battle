dead5fd1243cd61b9186794c13f1d4c9e2610b03
Michaël BOURMEAU <michael.bourmeau@outlook.fr>
2018-10-29 15:22:59 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.80e9d45bf77008415106bdb96629332f.js"
+  "/reims-0918-js-wild-battle/precache-manifest.62cf8c894e28ec8692fe25bb89bddcd9.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/reims-0918-js-wild-battle/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
