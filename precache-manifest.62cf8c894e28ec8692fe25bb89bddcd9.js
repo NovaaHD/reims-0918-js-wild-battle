d819c2d33a45d5b0e21d91237f85b1339c901ab0
Michaël BOURMEAU <michael.bourmeau@outlook.fr>
2018-10-29 14:57:54 +0100

    Updates

---
+++ precache-manifest.62cf8c894e28ec8692fe25bb89bddcd9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "feb764c4a96a209fbaa3",
+    "url": "/reims-0918-js-wild-battle/static/css/main.a80be2db.chunk.css"
+  },
+  {
+    "revision": "feb764c4a96a209fbaa3",
+    "url": "/reims-0918-js-wild-battle/static/js/main.feb764c4.chunk.js"
+  },
+  {
+    "revision": "5ff667392f9e66e951a9",
+    "url": "/reims-0918-js-wild-battle/static/css/1.f03c5bcd.chunk.css"
+  },
+  {
+    "revision": "5ff667392f9e66e951a9",
+    "url": "/reims-0918-js-wild-battle/static/js/1.5ff66739.chunk.js"
+  },
+  {
+    "revision": "995e4d769c35e0b36c33",
+    "url": "/reims-0918-js-wild-battle/static/js/runtime~main.995e4d76.js"
+  },
+  {
+    "revision": "f721e8eba82a9476647225c6928058a3",
+    "url": "/reims-0918-js-wild-battle/static/media/logo.f721e8eb.svg"
+  },
+  {
+    "revision": "61c8ea6dc493d4da0a5e330753355215",
+    "url": "/reims-0918-js-wild-battle/static/media/circleBackground.61c8ea6d.svg"
+  },
+  {
+    "revision": "050f56ddb5df4ac6436497c102e58fe1",
+    "url": "/reims-0918-js-wild-battle/static/media/stats.050f56dd.png"
+  },
+  {
+    "revision": "0d2541273656e2e7bae7fb0832126da5",
+    "url": "/reims-0918-js-wild-battle/static/media/list.0d254127.png"
+  },
+  {
+    "revision": "40f5cf89f1b6563fbdbb510f39f6436f",
+    "url": "/reims-0918-js-wild-battle/static/media/fight.40f5cf89.png"
+  },
+  {
+    "revision": "5ccc34641313289faeb198b0bb0dcffa",
+    "url": "/reims-0918-js-wild-battle/index.html"
+  }
+];
